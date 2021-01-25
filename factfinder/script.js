ed7d87e777da773032194fd6bceaf4c73eb44aeb
mrboorman <77455457+mrboorman@users.noreply.github.com>
2021-01-24 20:18:48 -0800

    upload of factfinder

---
+++ factfinder/script.js
@@ -0,0 +1,25 @@
+var fb = document.querySelector("#factButtons");
+var fd = document.querySelector("#factDisplay");
+
+fb.f1.addEventListener("click",showFact1);
+fb.f2.addEventListener("click",showFact2);
+fb.f3.addEventListener("click",showFact3);
+fb.f4.addEventListener("click",showFact4);
+fb.f5.addEventListener("click",showFact5);
+
+
+function showFact1(){
+	fd.innerHTML = "A duel between three people is actually called a truel."
+}
+function showFact2(){
+	fd.innerHTML = "Most Muppets are left-handed. (Because most Muppeteers are right-handed, so they operate the head with their favoured hand.)"
+}
+function showFact3(){
+	fd.innerHTML = "The top of the Eiffel Tower leans away from the sun, as the metal facing the sun heats up and expands. It can move as much as 7 inches.<br><img src='https://cdn.pixabay.com/photo/2017/08/20/06/31/urban-2660727_960_720.jpg' alt=''>"
+}
+function showFact4(){
+	fd.innerHTML = "In Switzerland, it is illegal to own just one guinea pig. This is because guinea pigs are social animals, and they are considered victims of abuse if they are alone."
+}
+function showFact5(){
+	fd.innerHTML = "The collective name for a group of unicorns is called a blessing."
+}
